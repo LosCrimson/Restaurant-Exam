393295896d5924401daeee6a6ec3aa2d6ecf26cd
LosCrimson <lukas.suslavicius@gmail.com>
2023-01-17 20:39:32 +0200

    Add project files.

---
+++ Models/Menu.cs
@@ -0,0 +1,19 @@
+﻿namespace Restaurant.Models
+{
+    public class Menu
+    {
+        private int _id;
+        private string _item;
+        private double _price;
+        public int Id { get { return _id; } set { _id = value; } }
+        public string Item { get { return _item; } set { _item = value; } }
+        public double Price { get { return _price; } set { _price = value; } }
+
+        public Menu(int id, string item, double price) 
+        {
+            _id = id;
+            _item = item;
+            _price = price;
+        }
+    }
+}
---
+++ Models/Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class Table
+    {
+        private int _id;
+        private int _seatingCapacity;
+        private bool _occupied;
+
+        public int Id { get { return _id;} set { _id = value; } }
+        public int SeatingCapacity { get { return _seatingCapacity; } set { _seatingCapacity = value; } }
+        public bool Occupied { get { return _occupied; } set { _occupied = value; } }
+
+        public Table(int id, int seatingCapacity, bool occupied) 
+        {
+            _id = id;
+            _seatingCapacity = seatingCapacity;
+            _occupied = occupied;
+        }
+    }
+}
