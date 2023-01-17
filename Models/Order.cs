f2068b4ac332cb8c0a663856044871d3b460990f
LosCrimson <lukas.suslavicius@gmail.com>
2023-01-17 20:48:14 +0200

    Made the bare minimum object needed for the project.

--- Models/Order.cs
+++ Models/Order.cs
@@ -9,5 +9,24 @@ namespace Restaurant.Models
     public class Order
     {
         private int _id;
+        private int _table;
+        private List<Menu> _menu;
+        private double _sum;
+        private DateTime _date;
+
+        public int Id { get { return _id; } set { _id = value; } }
+        public int Table { get { return _table;} set { _table = value; } }
+        public List<Menu> Menu { get { return _menu;} set { _menu = value; } }
+        public double Sum { get { return _sum;} set { _sum = value; } }
+        public DateTime Date { get { return _date;} set { _date = value; } }
+
+        public Order(int id, int table, List<Menu> menu, double sum, DateTime date) 
+        {
+            _id = id;
+            _table = table;
+            _menu = menu;
+            _sum = sum;
+            _date = date;
+        }
     }
 }
